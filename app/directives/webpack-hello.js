f286813e05474c56005c13e2e010ae8ef8349500
silasmartinez <silasm@gmail.com>
2015-08-17 13:47:11 -0600

    Initial Commit

---
+++ app/directives/webpack-hello.js
@@ -0,0 +1,16 @@
+export default ngModule => {
+  ngModule.directive('webpackHello', () => {
+    require('./webpack-hello.css');
+    return {
+      restrict: 'E',
+      scope: {},
+      template: require('./webpack-hello.html'),
+      controllerAs: 'vm',
+      controller: function() {
+        const vm = this;
+
+        vm.greeting = 'Hello Webpack';
+      }
+    };
+  });
+};
